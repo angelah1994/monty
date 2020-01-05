4a63cbd9f0fd281d4f20a41ef0df8ec413938df6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-05 01:03:20 -0500

    Prints only the top character of the stack

--- op_pchar.c
+++ op_pchar.c
@@ -9,24 +9,11 @@
   */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	stack_t *curr = NULL;
+	if (*stack == NULL)
+		handle_error(ERR_PCH_EMP, NULL, line_number, NULL);
 
-	if (*stack)
-	{
-		curr = *stack;
-
-		while (curr != NULL)
-		{
-			if (curr->n >= 0 && curr->n <= 127)
-				printf("%c\n", curr->n);
-			else
-				handle_error(ERR_PCH_USG, NULL, line_number, NULL);
-
-			curr = curr->next;
-		}
-
-		return;
-	}
-
-	handle_error(ERR_PCH_EMP, NULL, line_number, NULL);
+	if ((*stack)->n >= 0 && (*stack)->n <= 127)
+		printf("%c\n", (*stack)->n);
+	else
+		handle_error(ERR_PCH_USG, NULL, line_number, NULL);
 }
