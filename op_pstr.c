c35a05307ef221c27f27b8e12ae6204c1c6e5cf7
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-05 02:27:26 -0500

    Check if the ascii value is out of range

--- op_pstr.c
+++ op_pstr.c
@@ -19,6 +19,8 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 		if (curr->n > 0 && curr->n <= 127)
 			printf("%c", curr->n);
+		else
+			handle_error(ERR_PCH_USG, NULL, line_number, NULL);
 
 		curr = curr->next;
 	}
