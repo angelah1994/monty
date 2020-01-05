760ccb2d6848cde1bcacd82393d87fdfcfc3dde0
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-01-05 13:06:37 -0500

    Creates again the op_rotr function

--- op_rotl.c
+++ op_rotl.c
@@ -24,38 +24,3 @@ void rotl(stack_t **stack, unsigned int line_number)
 		}
 	}
 }
-
-/**
-  * rotr - ...
-  * @stack: ...
-  * @line_number: ...
-  *
-  * Return: ...
-  */
-void rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *curr = NULL, *last = NULL;
-	(void) line_number;
-	unsigned int i = 0, length = 0, temp = 0;
-
-	length = count_stack(*stack);
-
-	if (*stack)
-	{
-		curr = *stack;
-		last = *stack;
-
-		while (last->next != NULL)
-			last = last->next;
-
-		while (i < (length / 2))
-		{
-			temp = curr->n;
-			curr->n = last->n;
-			last->n = temp;
-			curr = curr->next;
-			last = last->prev;
-			++i;
-		}
-	}
-}
---
+++ op_rotr.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+  * rotr - ...
+  * @stack: ...
+  * @line_number: ...
+  *
+  * Return: ...
+  */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curr = NULL, *last = NULL;
+	(void) line_number;
+	unsigned int i = 0, length = 0, temp = 0;
+
+	length = count_stack(*stack);
+
+	if (*stack)
+	{
+		curr = *stack;
+		last = *stack;
+
+		while (last->next != NULL)
+			last = last->next;
+
+		while (i < (length / 2))
+		{
+			temp = curr->n;
+			curr->n = last->n;
+			last->n = temp;
+			curr = curr->next;
+			last = last->prev;
+			++i;
+		}
+	}
+}
